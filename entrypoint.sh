01e6d3ef7651a4a8fcdd45abafd3cdca4bc7518c
Sam Møller <smo@praxis.dk>
2022-10-03 14:24:02 +0200

    Fix unable to find container name or ID

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,6 @@ if [ -z "$INPUT_MSSQL_DATABASE" ]; then
     INPUT_MSSQL_DATABASE=test
 fi
 
-sh -c "docker run --name mssql -p $INPUT_HOST_PORT:$INPUT_CONTAINER_PORT -d mcr.microsoft.com/mssql/server:$INPUT_MSSQL_VERSION -e MSSQL_SA_PASSWORD=$INPUT_MSSQL_ROOT_PASSWORD -e ACCEPT_EULA=y"
-sh -c "docker exec mssql /opt/mssql-tools/bin/sqlcmd -U sa -P $INPUT_MSSQL_ROOT_PASSWORD -Q 'CREATE DATABASE $INPUT_MSSQL_DATABASE'"
+docker_runner=$(docker run --name mssql -p $INPUT_HOST_PORT:$INPUT_CONTAINER_PORT -e MSSQL_SA_PASSWORD=$INPUT_MSSQL_ROOT_PASSWORD -e ACCEPT_EULA=y -d mcr.microsoft.com/mssql/server:$INPUT_MSSQL_VERSION)
+docker_exec="docker exec $docker_runner /opt/mssql-tools/bin/sqlcmd -U sa -P $INPUT_MSSQL_ROOT_PASSWORD -Q 'CREATE DATABASE $INPUT_MSSQL_DATABASE'"
+sh -c "$docker_exec"
